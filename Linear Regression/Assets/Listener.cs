fdca851c0f1b5d949ec999311c932c6081798fba
TimmPure <T.j.m.voskuil@gmail.com>
2018-07-30 21:13:35 +0200

    Clicking spawns points at mouse position

---
+++ Linear Regression/Assets/Listener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour {
+
+    public Transform pointParent;
+    public GameObject pointPrefab;
+
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 clickPos;
+            clickPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f));
+            Instantiate(pointPrefab, clickPos, Quaternion.identity, pointParent);
+        }
+	}
+}
