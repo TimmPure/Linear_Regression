a6928e72ea93146060dd43fe7916adf162a552bc
TimmPure <T.j.m.voskuil@gmail.com>
2018-07-30 21:29:27 +0200

    Created a bounding box, outside which points won't spawn

--- Linear Regression/Assets/Listener.cs
+++ Linear Regression/Assets/Listener.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Listener : MonoBehaviour {
+public class Listener : MonoBehaviour
+{
 
     public Transform pointParent;
     public GameObject pointPrefab;
 
-	void Update () {
+    void Update()
+    {
         if (Input.GetMouseButtonUp(0))
         {
-            Vector3 clickPos;
-            clickPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f));
-            Instantiate(pointPrefab, clickPos, Quaternion.identity, pointParent);
+            Vector3 clickPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5f));
+            if (Mathf.Abs(clickPos.y) <= 10f && Mathf.Abs(clickPos.x) <= 10f)
+            {
+                Instantiate(pointPrefab, clickPos, Quaternion.identity, pointParent);
+            }
+            else
+            {
+                Debug.Log("Clicked outside the counding box");
+            }
         }
-	}
+    }
 }
